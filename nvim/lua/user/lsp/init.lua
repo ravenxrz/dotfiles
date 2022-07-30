86e76301051ef572660f6f017272129b1eb8c7ea
ravenxrz <zhang.xingrui@foxmail.com>
2022-07-30 17:11:43 +0800

    add new lsp config setup

--- nvim/lua/user/lsp/init.lua
+++ nvim/lua/user/lsp/init.lua
@@ -3,7 +3,9 @@ if not status_ok then
 	return
 end
 
-require("user.lsp.lsp-installer")
+-- require("user.lsp.lsp-installer")
+-- 和上面的配置二选一
+require("user.lsp.lsp-config-manually")
 require("user.lsp.handlers").setup()
 -- require("user.lsp.null-ls")
 -- require("user.lsp.lsp-utils")
---
+++ nvim/lua/user/lsp/lsp-config-manually.lua
@@ -0,0 +1,27 @@
+-- 如果lsp-installer无法下载，可以使用本文件手动配置lsp server
+local status_ok, lspconfig = pcall(require, "lspconfig")
+if not status_ok then
+  vim.notify("lspconfig not found!")
+  return
+end
+
+-- local on_attach = require("user.lsp.handlers").on_attach,
+-- local capabilities = require("user.lsp.handlers").capabilities,
+-- local lsp_flags = {
+--   -- This is the default in Nvim 0.7+
+--   debounce_text_changes = 150,
+-- }
+
+local opts = {
+  on_attach = require("user.lsp.handlers").on_attach,
+  capabilities = require("user.lsp.handlers").capabilities,
+  flags = {
+    debounce_text_changes = 150,
+  }
+}
+
+local clangd_opts = require("user.lsp.settings.clangd")
+opts = vim.tbl_deep_extend("force", clangd_opts, opts)
+lspconfig.clangd.setup{
+  opts
+}
