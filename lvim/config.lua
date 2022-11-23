fd5bb7a2b0b7f311c226ed9eeb3b6653545a7fb6
ravenxrz <zhang.xingrui@foxmail.com>
2022-11-23 19:08:34 +0800

    update configs

--- lvim/config.lua
+++ lvim/config.lua
@@ -94,6 +94,7 @@ lvim.builtin.which_key.mappings["t"] = {
   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
   w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
+  t = { "<cmd>TodoTrouble<cr>", "Todo" }
 }
 
 -- TODO: User Config for predefined plugins
@@ -105,13 +106,6 @@ lvim.builtin.nvimtree.setup.view.side = "right"
 lvim.builtin.nvimtree.setup.view.adaptive_size = true
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 lvim.builtin.cmp.cmdline.enable = true
--- add cmdline prompt, seems doesn't work
-for _, value in ipairs(lvim.builtin.cmp.cmdline.options) do
-  if value.type == ":" then
-    table.insert(value.sources, { name = "cmdline" })
-    break
-  end
-end
 lvim.builtin.gitsigns.opts.current_line_blame = true
 lvim.builtin.gitsigns.opts.current_line_blame_opts.delay = 200
 
@@ -174,13 +168,9 @@ end, lvim.lsp.automatic_configuration.skipped_servers)
 
 -- -- you can set a custom on_attach function that will be used for all the language servers
 -- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
--- lvim.lsp.on_attach_callback = function(client, bufnr)
---   local function buf_set_option(...)
---     vim.api.nvim_buf_set_option(bufnr, ...)
---   end
---   --Enable completion triggered by <c-x><c-o>
---   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
--- end
+lvim.lsp.on_attach_callback = function(client, bufnr)
+  require "lsp_signature".on_attach()
+end
 
 -- -- set a formatter, this will override the language server formatting capabilities (if it exists)
 -- local formatters = require "lvim.lsp.null-ls.formatters"
@@ -307,12 +297,73 @@ lvim.plugins = {
   { -- log file content highlighting
     "mtdl9/vim-log-highlighting"
   },
-  {
+  { -- theme
     "morhetz/gruvbox"
   },
-  {
-    'kevinhwang91/promise-async'
+  { -- auto save
+    "pocco81/auto-save.nvim"
   },
+  {
+    "folke/todo-comments.nvim",
+    config = function()
+      require("todo-comments").setu {}
+    end
+  },
+  {
+    -- show function signature when typing
+    "ray-x/lsp_signature.nvim",
+    config = function()
+      require "lsp_signature".setup({
+        debug = false, -- set to true to enable debug logging
+        log_path = vim.fn.stdpath("cache") .. "/lsp_signature.log", -- log dir when debug is on
+        -- default is  ~/.cache/nvim/lsp_signature.log
+        verbose = false, -- show debug line number
+
+        bind = true, -- This is mandatory, otherwise border config won't get registered.
+        -- If you want to hook lspsaga or other signature handler, pls set to false
+        doc_lines = 10, -- will show two lines of comment/doc(if there are more than two lines in doc, will be truncated);
+        -- set to 0 if you DO NOT want any API comments be shown
+        -- This setting only take effect in insert mode, it does not affect signature help in normal
+        -- mode, 10 by default
+
+        floating_window = true, -- show hint in a floating window, set to false for virtual text only mode
+
+        floating_window_above_cur_line = true, -- try to place the floating above the current line when possible Note:
+        -- will set to true when fully tested, set to false will use whichever side has more space
+        -- this setting will be helpful if you do not want the PUM and floating win overlap
+
+        floating_window_off_x = 1, -- adjust float windows x position.
+        floating_window_off_y = 1, -- adjust float windows y position.
+
+
+        fix_pos = false, -- set to true, the floating window will not auto-close until finish all parameters
+        hint_enable = true, -- virtual hint enable
+        hint_prefix = "🐼 ", -- Panda for parameter
+        hint_scheme = "String",
+        hi_parameter = "LspSignatureActiveParameter", -- how your parameter will be highlight
+        max_height = 12, -- max height of signature floating_window, if content is more than max_height, you can scroll down
+        -- to view the hiding contents
+        max_width = 80, -- max_width of signature floating_window, line will be wrapped if exceed max_width
+        handler_opts = {
+          border = "rounded" -- double, rounded, single, shadow, none
+        },
+
+        always_trigger = false, -- sometime show signature on new line or in middle of parameter can be confusing, set it to false for #58
+
+        auto_close_after = nil, -- autoclose signature float win after x sec, disabled if nil.
+        extra_trigger_chars = {}, -- Array of extra characters that will trigger signature completion, e.g., {"(", ","}
+        zindex = 200, -- by default it will be on top of all floating windows, set to <= 50 send it to bottom
+
+        padding = '', -- character to pad on left and right of signature can be ' ', or '|'  etc
+
+        transparency = nil, -- disabled by default, allow floating win transparent value 1~100
+        shadow_blend = 36, -- if you using shadow as border use this set the opacity
+        shadow_guibg = 'Black', -- if you using shadow as border use this set the color e.g. 'Green' or '#121315'
+        timer_interval = 200, -- default timer check interval set to lower value if you want to reduce latency
+        toggle_key = nil -- toggle signature on and off in insert mode,  e.g. toggle_key = '<M-x>'
+      })
+    end
+  }
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
