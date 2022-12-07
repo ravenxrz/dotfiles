7d0c13c6dd07ce172b547436f83eb3eb891b2c52
zhangxingrui <zhangxingrui.leo@bytedance.com>
2022-12-07 15:06:14 +0800

    update pyright config

--- lvim/config.lua
+++ lvim/config.lua
@@ -9,8 +9,7 @@ an executable
 
 -- general
 lvim.log.level = "warn"
-lvim.format_on_save.enabled = false
--- lvim.colorscheme = "lunar"
+lvim.format_on_save.enabled = true -- lvim.colorscheme = "lunar"
 -- themes: https://vimcolorschemes.com/
 lvim.colorscheme = "gruvbox"
 -- to disable icons and use a minimalist setup, uncomment the following
@@ -155,12 +154,24 @@ lvim.lsp.installer.setup.ensure_installed = {
 -- ---@usage disable automatic installation of servers
 lvim.lsp.installer.setup.automatic_installation = false
 -- disable diagnostics which is super annoying in my case
-vim.lsp.handlers["textDocument/publishDiagnostics"] = function() end
+-- vim.lsp.handlers["textDocument/publishDiagnostics"] = function() end
 
 -- ---configure a server manually. !!Requires `:LvimCacheReset` to take effect!!
 -- ---see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
-vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "clangd" })
+-- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "clangd" })
 -- local opts = {} -- check the lspconfig documentation for a list of all possible options
+require("lvim.lsp.manager").setup("pyright", {
+  settings = {
+    python = {
+      analysis = {
+        autoSearchPaths = true,
+        diagnosticMode = "workspace",
+        useLibraryCodeForTypes = true,
+        typeCheckingMode = "off"
+      }
+    }
+  },
+})
 
 
 -- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. !!Requires `:LvimCacheReset` to take effect!!
@@ -171,9 +182,9 @@ vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "clangd" })
 
 -- -- you can set a custom on_attach function that will be used for all the language servers
 -- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
-lvim.lsp.on_attach_callback = function(client, bufnr)
-  -- require "lsp_signature".on_attach()
-end
+-- lvim.lsp.on_attach_callback = function(client, bufnr)
+-- require "lsp_signature".on_attach()
+-- end
 
 -- -- set a formatter, this will override the language server formatting capabilities (if it exists)
 local formatters = require "lvim.lsp.null-ls.formatters"
@@ -233,7 +244,10 @@ lvim.plugins = {
   { -- only works on https://github.com/universal-ctags/ctags
     "liuchengxu/vista.vim",
     config = function()
-      vim.cmd([[ let g:vista_sidebar_position = 'vertical topleft' ]])
+      vim.cmd([[ 
+      let g:vista_sidebar_position = 'vertical topleft' 
+      let g:vista_default_executive = 'nvim_lsp' 
+      ]])
     end
   },
   { -- telescope instant searching
@@ -311,61 +325,6 @@ lvim.plugins = {
   },
   { -- auto save
     "pocco81/auto-save.nvim"
-  },
-  {
-    -- -- show function signature when typing
-    -- "ray-x/lsp_signature.nvim",
-    -- config = function()
-    --   require "lsp_signature".setup({
-    --     debug = false, -- set to true to enable debug logging
-    --     log_path = vim.fn.stdpath("cache") .. "/lsp_signature.log", -- log dir when debug is on
-    --     -- default is  ~/.cache/nvim/lsp_signature.log
-    --     verbose = false, -- show debug line number
-
-    --     bind = true, -- This is mandatory, otherwise border config won't get registered.
-    --     -- If you want to hook lspsaga or other signature handler, pls set to false
-    --     doc_lines = 10, -- will show two lines of comment/doc(if there are more than two lines in doc, will be truncated);
-    --     -- set to 0 if you DO NOT want any API comments be shown
-    --     -- This setting only take effect in insert mode, it does not affect signature help in normal
-    --     -- mode, 10 by default
-
-    --     floating_window = true, -- show hint in a floating window, set to false for virtual text only mode
-
-    --     floating_window_above_cur_line = true, -- try to place the floating above the current line when possible Note:
-    --     -- will set to true when fully tested, set to false will use whichever side has more space
-    --     -- this setting will be helpful if you do not want the PUM and floating win overlap
-
-    --     floating_window_off_x = 1, -- adjust float windows x position.
-    --     floating_window_off_y = 1, -- adjust float windows y position.
-
-
-    --     fix_pos = false, -- set to true, the floating window will not auto-close until finish all parameters
-    --     hint_enable = true, -- virtual hint enable
-    --     hint_prefix = "🐼 ", -- Panda for parameter
-    --     hint_scheme = "String",
-    --     hi_parameter = "LspSignatureActiveParameter", -- how your parameter will be highlight
-    --     max_height = 12, -- max height of signature floating_window, if content is more than max_height, you can scroll down
-    --     -- to view the hiding contents
-    --     max_width = 80, -- max_width of signature floating_window, line will be wrapped if exceed max_width
-    --     handler_opts = {
-    --       border = "rounded" -- double, rounded, single, shadow, none
-    --     },
-
-    --     always_trigger = false, -- sometime show signature on new line or in middle of parameter can be confusing, set it to false for #58
-
-    --     auto_close_after = nil, -- autoclose signature float win after x sec, disabled if nil.
-    --     extra_trigger_chars = {}, -- Array of extra characters that will trigger signature completion, e.g., {"(", ","}
-    --     zindex = 200, -- by default it will be on top of all floating windows, set to <= 50 send it to bottom
-
-    --     padding = '', -- character to pad on left and right of signature can be ' ', or '|'  etc
-
-    --     transparency = nil, -- disabled by default, allow floating win transparent value 1~100
-    --     shadow_blend = 36, -- if you using shadow as border use this set the opacity
-    --     shadow_guibg = 'Black', -- if you using shadow as border use this set the color e.g. 'Green' or '#121315'
-    --     timer_interval = 200, -- default timer check interval set to lower value if you want to reduce latency
-    --     toggle_key = nil -- toggle signature on and off in insert mode,  e.g. toggle_key = '<M-x>'
-    --   })
-    -- end
   },
   {
     "p00f/clangd_extensions.nvim",
