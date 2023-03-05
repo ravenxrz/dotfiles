0b495668f6301abf51711ef5d589484445f5c99a
Raven <zhang.xingrui@foxmail.com>
2023-03-05 11:48:24 +0800

    attach a tmux session or create a tmux session automatically when login

--- shell/autocommands.sh
+++ shell/autocommands.sh
@@ -4,3 +4,7 @@
 # if command -v tmux &> /dev/null && [ -z "$TMUX" ]; then
 #     tmux attach -t default || tmux new -s default
 # fi
+
+if [[ $- =~ i ]] && [[ -z "$TMUX" ]] && [[ -n "$SSH_TTY" ]]; then
+  tmux at || tmux new-session
+fi
