c90ef28333e3ad328a38859b17f1f8099bdc42bc
zhangxingrui <zhangxingrui.leo@bytedance.com>
2024-02-24 17:24:05 +0800

    add auto-pair

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -157,5 +157,24 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
     }
+  },
+  {
+    'windwp/nvim-autopairs',
+    dependencies = {
+      'hrsh7th/nvim-cmp', -- Autocompletion plugin
+    },
+    event = "InsertEnter",
+    config = function()
+      require('nvim-autopairs').setup({
+        disable_filetype = { "TelescopePrompt", "vim", "spectre_panel" },
+      })
+      -- add auto pair when auto completion down
+      local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+      local cmp = require('cmp')
+      cmp.event:on(
+        'confirm_done',
+        cmp_autopairs.on_confirm_done()
+      )
+    end
   }
 }
