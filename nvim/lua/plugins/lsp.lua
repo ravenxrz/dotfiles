6c301fda69c4913b2603d86c55fad40278aeafe9
zhangxingrui <zhangxingrui.leo@bytedance.com>
2024-02-24 18:20:12 +0800

    fix lsp conf

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -8,6 +8,7 @@ return {
     -- setup lsp
     -- see: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
     -- for supported languages
+    -- refactor config to get better scalability
     local capabilities = require("cmp_nvim_lsp").default_capabilities()
     lspconfig.pyright.setup {
       capabilities = capabilities,
@@ -71,16 +72,16 @@ return {
         vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
         vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
         vim.keymap.set('n', 'gh', vim.lsp.buf.signature_help, opts)
-        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-        vim.keymap.set('n', '<space>wl', function()
+        vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+        vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+        vim.keymap.set('n', '<leader>wl', function()
           print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
         end, opts)
-        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-        vim.keymap.set({ 'n', 'v' }, '<space>la', vim.lsp.buf.code_action, opts)
+        -- vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+        vim.keymap.set({ 'n', 'v' }, '<leader>la', vim.lsp.buf.code_action, opts)
         -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-        vim.keymap.set({ 'n', 'v' }, '<space>f', function()
+        vim.keymap.set({ 'n', 'v' }, '<leader>lf', function()
           vim.lsp.buf.format { async = true }
         end, opts)
       end,
