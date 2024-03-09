3ce44702f1556724b0448f1e1c1ce164354f4791
zhangxingrui <zhangxingrui.leo@bytedance.com>
2024-03-09 11:00:51 +0800

    add session manager

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -66,6 +66,7 @@ keymap("n", "<leader>fF", "<cmd>Telescope find_files find_command=rg,--no-ignore
 keymap("n", "<leader>fw", "<cmd>lua require('telescope-live-grep-args.shortcuts').grep_word_under_cursor()<cr>", opts)
 keymap("v", "<leader>fw", "<cmd>lua require('telescope-live-grep-args.shortcuts').grep_word_under_cursor()<cr>", opts)
 keymap("n", "<leader>fg", "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<cr>", opts)
+-- TODO: add telescope raw live_grep shortucts
 keymap("n", "<leader>s", "<cmd>Telescope lsp_document_symbols<cr>", opts)
 keymap("n", "<leader>S", "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>", opts)
 keymap("n", "gr", "<cmd>Telescope lsp_references<cr>", opts)
@@ -79,6 +80,10 @@ keymap("n", "<leader>gg", "<cmd>LazyGit<cr>", opts)
 
 -- spectre config
 keymap("n", "<leader>F", "<cmd>lua require('spectre').open_file_search({select_word=true})<cr>", opts)
+
+-- session manager
+keymap("n", "<leader>P", "<cmd>SessionManager! load_session<cr>", opts)
+
 
 -- cppp header/source switch
 keymap("n", "<leader>j", "<cmd>ClangdSwitchSourceHeader<cr>", opts)
