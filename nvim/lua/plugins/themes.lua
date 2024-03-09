dad555ae7deb9efab193cf47d05c7bc95f47babd
zhangxingrui <zhangxingrui.leo@bytedance.com>
2024-03-09 10:28:21 +0800

    use tinted theme

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -2,12 +2,28 @@ return {
   {
     'Mofiqul/dracula.nvim',
     priority = 1000,
-    config = function()
-      vim.cmd([[colorscheme dracula]])
-    end
   },
   {
     "miikanissi/modus-themes.nvim",
     priority = 1000,
+    config = function()
+      require("modus-themes").setup({
+        -- Theme comes in two styles `modus_operandi` and `modus_vivendi`
+        -- `auto` will automatically set style based on background set with vim.o.background
+        style = "modus_operandi",
+        variant = "tinted",   -- Theme comes in four variants `default`, `tinted`, `deuteranopia`, and `tritanopia`
+        transparent = false,  -- Transparent background (as supported by the terminal)
+        dim_inactive = false, -- "non-current" windows are dimmed
+        styles = {
+          -- Style to be applied to different syntax groups
+          -- Value is any valid attr-list value for `:help nvim_set_hl`
+          comments = { italic = true },
+          keywords = { italic = true },
+          functions = {},
+          variables = {},
+        },
+      })
+      vim.cmd([[colorscheme modus_operandi]])
+    end
   },
 }
