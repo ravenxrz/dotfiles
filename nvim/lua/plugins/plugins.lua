1cdd6b31f5f53880b210a52fc6909d7f0cba562e
zhangxingrui <zhangxingrui.leo@bytedance.com>
2024-03-09 10:46:29 +0800

    use better winbar

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -368,32 +368,19 @@ return {
         },
         tabline = {},
         winbar = {
-          lualine_c = {
-            "navic",
-            -- Component specific options
-            color_correction = nil, -- Can be nil, "static" or "dynamic". This option is useful only when you have highlights enabled.
-            -- Many colorschemes don't define same backgroud for nvim-navic as their lualine statusline backgroud.
-            -- Setting it to "static" will perform a adjustment once when the component is being setup. This should
-            --   be enough when the lualine section isn't changing colors based on the mode.
-            -- Setting it to "dynamic" will keep updating the highlights according to the current modes colors for
-            --   the current section.
-            navic_opts = nil -- lua table with same format as setup's option. All options except "lsp" options take effect when set here.
-          },
+          -- lualine_c = {
+          --   "navic",
+          --   -- Component specific options
+          --   color_correction = nil, -- Can be nil, "static" or "dynamic". This option is useful only when you have highlights enabled.
+          --   -- Many colorschemes don't define same backgroud for nvim-navic as their lualine statusline backgroud.
+          --   -- Setting it to "static" will perform a adjustment once when the component is being setup. This should
+          --   --   be enough when the lualine section isn't changing colors based on the mode.
+          --   -- Setting it to "dynamic" will keep updating the highlights according to the current modes colors for
+          --   --   the current section.
+          --   navic_opts = nil -- lua table with same format as setup's option. All options except "lsp" options take effect when set here.
+          -- },
         },
-        inactive_winbar = {
-          lualine_c = {
-            "navic",
-            -- Component specific options
-            color_correction = nil, -- Can be nil, "static" or "dynamic". This option is useful only when you have highlights enabled.
-            -- Many colorschemes don't define same backgroud for nvim-navic as their lualine statusline backgroud.
-            -- Setting it to "static" will perform a adjustment once when the component is being setup. This should
-            --   be enough when the lualine section isn't changing colors based on the mode.
-            -- Setting it to "dynamic" will keep updating the highlights according to the current modes colors for
-            --   the current section.
-            navic_opts = nil -- lua table with same format as setup's option. All options except "lsp" options take effect when set here.
-          },
-
-        },
+        inactive_winbar = {},
         extensions = {}
       }
     end
@@ -445,6 +432,18 @@ return {
         return text
       end,
     }
+  },
+  {
+    "utilyre/barbecue.nvim",
+    name = "barbecue",
+    version = "*",
+    dependencies = {
+      "SmiteshP/nvim-navic",
+      "nvim-tree/nvim-web-devicons", -- optional dependency
+    },
+    opts = {
+      -- configurations go here
+    },
   },
   {
     "okuuva/auto-save.nvim",
