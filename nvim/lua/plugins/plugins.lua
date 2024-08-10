6ed9bbb429784448e50b3fa351ec1ce15442e2d3
zhangxingrui <zhangxingrui.leo@bytedance.com>
2024-08-10 19:09:31 +0800

    add git conflict plugin

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -815,4 +815,26 @@ return {
 			})
 		end,
 	},
+	{
+		"akinsho/git-conflict.nvim",
+		version = "*",
+		config = true,
+		opts = {
+			default_mappings = {
+				ours = "co",
+				theirs = "ct",
+				none = "c0",
+				both = "cb",
+				next = "]x",
+				prev = "[x",
+			}, -- disable buffer local mapping created by this plugin
+			default_commands = true, -- disable commands created by this plugin
+			disable_diagnostics = false, -- This will disable the diagnostics in a buffer whilst it is conflicted
+			list_opener = "copen", -- command or function to open the conflicts list
+			highlights = { -- They must have background color, otherwise the default color will be used
+				incoming = "DiffAdd",
+				current = "DiffText",
+			},
+		},
+	},
 }
