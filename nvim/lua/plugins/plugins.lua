2bbe2adb3b1c7c913fc335165e8693c921a51d98
zhangxingrui <zhangxingrui.leo@bytedance.com>
2024-08-10 19:09:37 +0800

    add nvim context vt

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -669,6 +669,45 @@ return {
 		end,
 	},
 	{
+		"andersevenrud/nvim_context_vt",
+		event = "VeryLazy",
+		config = function()
+			require("nvim_context_vt").setup({
+				-- Enable by default. You can disable and use :NvimContextVtToggle to maually enable.
+				-- Default: true
+				enabled = true,
+
+				-- Override default virtual text prefix
+				-- Default: '-->'
+				prefix = "",
+
+				-- Override the internal highlight group name
+				-- Default: 'ContextVt'
+				highlight = "ContextVt",
+
+				-- Disable virtual text for given filetypes
+				-- Default: { 'markdown' }
+				disable_ft = { "markdown" },
+
+				-- Disable display of virtual text below blocks for indentation based languages like Python
+				-- Default: false
+				disable_virtual_lines = false,
+
+				-- Same as above but only for spesific filetypes
+				-- Default: {}
+				disable_virtual_lines_ft = { "yaml" },
+
+				-- How many lines required after starting position to show virtual text
+				-- Default: 1 (equals two lines total)
+				min_rows = 5,
+
+				-- Same as above but only for spesific filetypes
+				-- Default: {}
+				min_rows_ft = {},
+			})
+		end,
+	},
+	{
 		"nvim-treesitter/nvim-treesitter",
 		commit = "1b050206e490a4146cdf25c7b38969c1711b5620",
 		build = ":TSUpdate",
