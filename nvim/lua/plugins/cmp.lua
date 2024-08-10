9049703aae6d90049b4755ed48231d2b708cdf4a
zhangxingrui <zhangxingrui.leo@bytedance.com>
2024-08-10 18:46:19 +0800

    add neogen

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,118 +1,127 @@
 return {
-	{
-		"saadparwaiz1/cmp_luasnip", -- Snippets source for nvim-cmp
-		event = "InsertEnter",
-	},
-	{
-		"L3MON4D3/LuaSnip", -- Snippets plugin
-		dependencies = { "rafamadriz/friendly-snippets" },
-		event = "InsertEnter",
-		config = function()
-			require("luasnip.loaders.from_vscode").lazy_load() -- load freindly-snippets
-		end,
-	},
-	{
-		"hrsh7th/cmp-nvim-lsp", -- LSP source for nvim-cmp
-		event = "InsertEnter",
-	},
-	{
-		"hrsh7th/nvim-cmp", -- Autocompletion plugin
-		event = "InsertEnter",
-		dependencies = {
-			"hrsh7th/cmp-nvim-lsp",
-			"neovim/nvim-lspconfig",
-			-- "hrsh7th/cmp-nvim-lsp-signature-help",
-			"hrsh7th/cmp-path",
-			"hrsh7th/cmp-buffer",
-			"onsails/lspkind.nvim",
-		},
-		config = function()
-			local luasnip = require("luasnip")
-			local cmp = require("cmp")
-			local lspkind = require("lspkind")
+ {
+  "saadparwaiz1/cmp_luasnip", -- Snippets source for nvim-cmp
+  event = "InsertEnter",
+ },
+ {
+  "L3MON4D3/LuaSnip", -- Snippets plugin
+  dependencies = { "rafamadriz/friendly-snippets" },
+  event = "InsertEnter",
+  config = function()
+   require("luasnip.loaders.from_vscode").lazy_load() -- load freindly-snippets
+  end,
+ },
+ {
+  "hrsh7th/cmp-nvim-lsp", -- LSP source for nvim-cmp
+  event = "InsertEnter",
+ },
 
-			local select_next = cmp.mapping(function(fallback)
-				if cmp.visible() then
-					cmp.select_next_item()
-				elseif luasnip.expand_or_jumpable() then
-					luasnip.expand_or_jump()
-				else
-					fallback()
-				end
-			end, { "i", "s" })
-			local select_prev = cmp.mapping(function(fallback)
-				if cmp.visible() then
-					cmp.select_prev_item()
-				elseif luasnip.jumpable(-1) then
-					luasnip.jump(-1)
-				else
-					fallback()
-				end
-			end, { "i", "s" })
+ {
+  "danymat/neogen",
+  cmd = { "Neogen" },
+  config = function()
+   require("neogen").setup({ snippet_engine = "luasnip" })
+  end,
+  version = "*",
+ },
+ {
+  "hrsh7th/nvim-cmp", -- Autocompletion plugin
+  event = "InsertEnter",
+  dependencies = {
+   "hrsh7th/cmp-nvim-lsp",
+   "neovim/nvim-lspconfig",
+   -- "hrsh7th/cmp-nvim-lsp-signature-help",
+   "hrsh7th/cmp-path",
+   "hrsh7th/cmp-buffer",
+   "onsails/lspkind.nvim",
+  },
+  config = function()
+   local luasnip = require("luasnip")
+   local cmp = require("cmp")
+   local lspkind = require("lspkind")
 
-			cmp.setup({
-				formatting = {
-					-- format = function(entry, vim_item)
-					-- 	vim_item.menu = ""
-					-- 	-- vim_item.kind = ""
-					-- 	vim_item.abbr = string.sub(vim_item.abbr, 1, 30)
-					-- 	return vim_item
-					-- end,
-					--
-					format = lspkind.cmp_format({
-						mode = "symbol_text", -- show only symbol annotations
-						maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-						-- can also be a function to dynamically calculate max width such as
-						-- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
-						ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-						show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+   local select_next = cmp.mapping(function(fallback)
+    if cmp.visible() then
+     cmp.select_next_item()
+    elseif luasnip.expand_or_jumpable() then
+     luasnip.expand_or_jump()
+    else
+     fallback()
+    end
+   end, { "i", "s" })
+   local select_prev = cmp.mapping(function(fallback)
+    if cmp.visible() then
+     cmp.select_prev_item()
+    elseif luasnip.jumpable(-1) then
+     luasnip.jump(-1)
+    else
+     fallback()
+    end
+   end, { "i", "s" })
 
-						-- The function below will be called before any actual modifications from lspkind
-						-- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-						before = function(entry, vim_item)
-							-- ...
-							return vim_item
-						end,
-					}),
-				},
-				snippet = {
-					expand = function(args)
-						luasnip.lsp_expand(args.body)
-					end,
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-u>"] = cmp.mapping.scroll_docs(-4), -- Up
-					["<C-d>"] = cmp.mapping.scroll_docs(4), -- Down
-					-- C-b (back) C-f (forward) for snippet placeholder navigation.
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<CR>"] = cmp.mapping.confirm({
-						select = true,
-						behavior = cmp.ConfirmBehavior.Insert,
-					}),
-					["<C-n>"] = select_next,
-					["<C-p>"] = select_prev,
-					["<Tab>"] = select_next,
-					["<S-Tab>"] = select_prev,
-				}),
-				sources = {
-					{ name = "nvim_lsp" },
-					{ name = "nvim_lsp_signature_help" },
-					{ name = "luasnip" },
-					{ name = "buffer" },
-					{ name = "path" },
-				},
-			})
-		end,
-	},
-	{
-		"rcarriga/cmp-dap",
-		event = "InsertEnter",
-		config = function()
-			require("cmp").setup.filetype({ "dap-repl", "dapui_watches", "dapui_hover" }, {
-				sources = {
-					{ name = "dap" },
-				},
-			})
-		end,
-	},
+   cmp.setup({
+    formatting = {
+     -- format = function(entry, vim_item)
+     -- 	vim_item.menu = ""
+     -- 	-- vim_item.kind = ""
+     -- 	vim_item.abbr = string.sub(vim_item.abbr, 1, 30)
+     -- 	return vim_item
+     -- end,
+     --
+     format = lspkind.cmp_format({
+      mode = "symbol_text", -- show only symbol annotations
+      maxwidth = 50,        -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+      -- can also be a function to dynamically calculate max width such as
+      -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+      ellipsis_char = "...",    -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+      show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+
+      -- The function below will be called before any actual modifications from lspkind
+      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+      before = function(entry, vim_item)
+       -- ...
+       return vim_item
+      end,
+     }),
+    },
+    snippet = {
+     expand = function(args)
+      luasnip.lsp_expand(args.body)
+     end,
+    },
+    mapping = cmp.mapping.preset.insert({
+     ["<C-u>"] = cmp.mapping.scroll_docs(-4), -- Up
+     ["<C-d>"] = cmp.mapping.scroll_docs(4),  -- Down
+     -- C-b (back) C-f (forward) for snippet placeholder navigation.
+     ["<C-Space>"] = cmp.mapping.complete(),
+     ["<CR>"] = cmp.mapping.confirm({
+      select = true,
+      behavior = cmp.ConfirmBehavior.Insert,
+     }),
+     ["<C-n>"] = select_next,
+     ["<C-p>"] = select_prev,
+     ["<Tab>"] = select_next,
+     ["<S-Tab>"] = select_prev,
+    }),
+    sources = {
+     { name = "nvim_lsp" },
+     { name = "nvim_lsp_signature_help" },
+     { name = "luasnip" },
+     { name = "buffer" },
+     { name = "path" },
+    },
+   })
+  end,
+ },
+ {
+  "rcarriga/cmp-dap",
+  event = "InsertEnter",
+  config = function()
+   require("cmp").setup.filetype({ "dap-repl", "dapui_watches", "dapui_hover" }, {
+    sources = {
+     { name = "dap" },
+    },
+   })
+  end,
+ },
 }
