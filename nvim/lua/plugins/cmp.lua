00cd2cb5baf6ea2b8459b9e1a346118a56d16634
zhangxingrui <zhangxingrui.leo@bytedance.com>
2024-08-10 22:01:11 +0800

    remove lspkint and ts context

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -33,12 +33,12 @@ return {
    -- "hrsh7th/cmp-nvim-lsp-signature-help",
    "hrsh7th/cmp-path",
    "hrsh7th/cmp-buffer",
-   "onsails/lspkind.nvim",
+   -- "onsails/lspkind.nvim",
   },
   config = function()
    local luasnip = require("luasnip")
    local cmp = require("cmp")
-   local lspkind = require("lspkind")
+   -- local lspkind = require("lspkind")
 
    local select_next = cmp.mapping(function(fallback)
     if cmp.visible() then
@@ -61,28 +61,13 @@ return {
 
    cmp.setup({
     formatting = {
-     -- format = function(entry, vim_item)
-     -- 	vim_item.menu = ""
-     -- 	-- vim_item.kind = ""
-     -- 	vim_item.abbr = string.sub(vim_item.abbr, 1, 30)
-     -- 	return vim_item
-     -- end,
-     --
-     format = lspkind.cmp_format({
-      mode = "symbol_text", -- show only symbol annotations
-      maxwidth = 50,        -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-      -- can also be a function to dynamically calculate max width such as
-      -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
-      ellipsis_char = "...",    -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-      show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+     format = function(entry, vim_item)
+     	vim_item.menu = ""
+     	-- vim_item.kind = ""
+     	vim_item.abbr = string.sub(vim_item.abbr, 1, 30)
+     	return vim_item
+     end,
 
-      -- The function below will be called before any actual modifications from lspkind
-      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-      before = function(entry, vim_item)
-       -- ...
-       return vim_item
-      end,
-     }),
     },
     snippet = {
      expand = function(args)
