9672a316f261bbc47a957d732cc95f1bd0ec7c07
zhangxingrui <zhangxingrui.leo@bytedance.com>
2025-02-06 10:24:53 +0800

    disable zsh auto correct, adjust auto-save condition

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -767,8 +767,8 @@ return {
       require("auto-save").setup({
         enabled = true,                                  -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
         trigger_events = {                               -- See :h events
-          immediate_save = { "BufLeave", "FocusLost" },  -- vim events that trigger an immediate save
-          defer_save = { "InsertLeave", "TextChanged" }, -- vim events that trigger a deferred save (saves after `debounce_delay`)
+          immediate_save = { "BufLeave", "FocusLost" , "InsertLeave" },  -- vim events that trigger an immediate save
+          defer_save = { "TextChanged" }, -- vim events that trigger a deferred save (saves after `debounce_delay`)
           cancel_deferred_save = { "InsertEnter" },      -- vim events that cancel a pending deferred save
         },
         -- function that takes the buffer handle and determines whether to save the current buffer or not
