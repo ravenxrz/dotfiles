156d7df96ea27c7171730ba5a8d09c4e27867406
zhangxingrui <zhangxingrui.leo@bytedance.com>
2025-02-07 16:54:12 +0800

    adjust blink show doc delay to 500ms

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -35,7 +35,7 @@ return {
       completion = {
         accept = { auto_brackets = { enabled = true }, },
         -- Show documentation when selecting a completion item
-        documentation = { auto_show = true, auto_show_delay_ms = 200 },
+        documentation = { auto_show = true, auto_show_delay_ms = 500 },
         -- Display a preview of the selected item on the current line
         ghost_text = { enabled = false },
       },
