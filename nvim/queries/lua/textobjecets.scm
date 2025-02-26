91a471820288eab8692c586fe167af2a82553135
zhangxingrui <zhangxingrui.leo@bytedance.com>
2025-02-26 21:53:53 +0800

    feat: add lua textobjects.scm

---
+++ nvim/queries/lua/textobjecets.scm
@@ -0,0 +1,22 @@
+; extends
+
+; 函数声明
+[
+;; local func
+local_declaration: (function_declaration 
+  name: (identifier) @custom.function.declare) 
+
+;; global func
+(function_declaration 
+  name: (method_index_expression
+    table: (identifier)
+    method: (identifier) @custom.function.declare))
+
+; 先声明，再赋值的func
+(assignment_statement 
+  (variable_list  
+    name: (identifier) @custom.function.declare)
+    (expression_list 
+      value: (function_definition)))
+]
+
