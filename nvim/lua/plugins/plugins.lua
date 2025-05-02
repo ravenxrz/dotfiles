f33965a104fe29840cef62cbb0d5752352876515
zhangxingrui <zhangxingrui.leo@bytedance.com>
2025-05-02 17:24:38 +0800

    nvim: remove zen-mode

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -80,46 +80,6 @@ return {
     },
   },
   {
-    "folke/zen-mode.nvim",
-    cmd = { "ZenMode" },
-    enabled = true,
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-      window = {
-        backdrop = 0.95, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
-        -- height and width can be:
-        -- * an absolute number of cells when > 1
-        -- * a percentage of the width / height of the editor when <= 1
-        -- * a function that returns the width or the height
-        width = 145, -- width of the Zen window
-        height = 1,  -- height of the Zen window
-        -- by default, no options are changed for the Zen window
-        -- uncomment any of the options below, or add other vim.wo options you want to apply
-        options = {
-          signcolumn = "yes",     -- disable signcolumn
-          number = true,          -- disable number column
-          relativenumber = false, -- disable relative numbers
-          cursorline = false,     -- disable cursorline
-          cursorcolumn = false,   -- disable cursor column
-          foldcolumn = "99",      -- disable fold column
-          list = false,           -- disable whitespace characters
-        },
-      },
-      plugins = {
-        options = {
-          enabled = true,
-          ruler = false,   -- disables the ruler text in the cmd line area
-          showcmd = false, -- disables the command in the last line of the screen
-          -- you may turn on/off statusline in zen mode by setting 'laststatus'
-          -- statusline will be shown only if 'laststatus' == 3
-          laststatus = 0, -- turn off the statusline in zen mode
-        },
-      },
-    },
-  },
-  {
     "ggandor/leap.nvim",
     config = function()
       -- require('leap').create_default_mappings()
@@ -170,12 +130,11 @@ return {
     cmd = {
       "Neotree",
     },
-    branch = "v3.x",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+      -- "3rd/image.nvim",              -- Optional image support in preview window: See `# Preview Mode` for more information
     },
     config = function()
       require("neo-tree").setup({
