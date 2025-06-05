41ca0c201e97c2964a0db0c60a3b5dfa9f15ea65
zhangxingrui <zhangxingrui.leo@bytedance.com>
2025-06-05 19:15:47 +0800

    nvim: show hidden files

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -132,6 +132,33 @@ return {
             leave_dirs_open = false,     -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
           },
           use_libuv_file_watcher = true, -- This will use the OS level file watchers to detect changes
+          filtered_items = {
+            visible = true,              -- when true, they will just be displayed differently than normal items
+            hide_dotfiles = true,
+            hide_gitignored = false,
+            hide_hidden = true, -- only works on Windows for hidden files/directories
+            hide_by_name = {
+              --"node_modules"
+            },
+            hide_by_pattern = { -- uses glob style patterns
+              --"*.meta",
+              --"*/src/*/tsconfig.json",
+              -- ".calltree.*",
+            },
+            always_show = { -- remains visible even if other settings would normally hide it
+              ".gitignored",
+            },
+            always_show_by_pattern = { -- uses glob style patterns
+              --".env*",
+            },
+            never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+              ".DS_Store",
+              "thumbs.db"
+            },
+            never_show_by_pattern = { -- uses glob style patterns
+              --".null-ls_*",
+            },
+          },
         },
         window = {
           position = "left",
