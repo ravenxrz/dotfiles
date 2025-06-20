72fd9310073b837b2f8b43434993f15b1a27836f
zhangxingrui <zhangxingrui.leo@bytedance.com>
2025-06-20 20:40:07 +0800

    nvim: add debugprint

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -1,5 +1,42 @@
 return {
   {
+    "andrewferrier/debugprint.nvim",
+    opts = {
+      keymaps = {
+        normal = {
+          plain_below = "g?p",
+          plain_above = "g?P",
+          variable_below = "g?v",
+          variable_above = "g?V",
+          variable_below_alwaysprompt = "",
+          variable_above_alwaysprompt = "",
+          surround_plain = "g?sp",
+          surround_variable = "g?sv",
+          surround_variable_alwaysprompt = "",
+          textobj_below = "g?o",
+          textobj_above = "g?O",
+          textobj_surround = "g?so",
+          toggle_comment_debug_prints = "",
+          delete_debug_prints = "",
+        },
+        insert = {
+          plain = "<C-G>p",
+          variable = "<C-G>v",
+        },
+        visual = {
+          variable_below = "g?v",
+          variable_above = "g?V",
+        },
+      },
+    },
+    dependencies = {
+      "echasnovski/mini.nvim",         -- Optional: Needed for line highlighting (full mini.nvim plugin)
+      "nvim-telescope/telescope.nvim", -- Optional: If you want to use the `:Debugprint search` command with telescope.nvim
+    },
+    lazy = false,                      -- Required to make line highlighting work before debugprint is first used
+    version = "*",                     -- Remove if you DON'T want to use the stable version
+  },
+  {
     "tamton-aquib/keys.nvim",
     opts = {
       enable_on_startup = false,
