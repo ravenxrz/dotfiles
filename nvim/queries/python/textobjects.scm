2a33f3d845d19afb834e9e5fd42c0320d159f6d9
zhangxingrui <zhangxingrui.leo@bytedance.com>
2025-11-28 16:11:51 +0800

    feat: better python treessiter parser

--- nvim/queries/python/textobjects.scm
+++ nvim/queries/python/textobjects.scm
@@ -1,3 +1,8 @@
 ; extends
 
-(function_definition name: (identifier) @custom.function.declare)
+(function_definition
+  name: (identifier) @custom.function.declare)
+
+(decorated_definition
+  definition: (function_definition
+    name: (identifier) @custom.function.declare))
